15ec510542a855a13300a0b4b60cef262c2ee6a5
Geror125 <65406662+Geror125@users.noreply.github.com>
2021-03-30 17:05:18 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {   
+    public static void main(String[] args) {
+        byte num = 32;
+        short num2 = 5418;
+        int num3 = 44484516;
+        long num4 = 1122334455;
+        float num5 = 3.484f;
+        double num6 = 3.46846848484d;
+        char sym = 't';
+        boolean bl = true;
+        System.out.println (num);
+        System.out.println (num2);
+        System.out.println (num3);
+        System.out.println (num4);
+        System.out.println (num5);
+        System.out.println (num6);
+        System.out.println (sym);
+        System.out.println (bl);
+    }
+}
