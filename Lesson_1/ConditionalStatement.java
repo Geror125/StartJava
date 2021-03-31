45694e687fcd24e251e01c0c00dca80eabebd9c3
Geror125 <65406662+Geror125@users.noreply.github.com>
2021-03-31 14:48:50 +0300

    Corrected errors

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,34 +1,33 @@
-public class ConditionalStatement{
-    public static void main(String[] args){
-
+public class ConditionalStatement {
+    public static void main(String[] args) {
         byte age = 23;
-        if ( age > 20) {
-            System.out.println( "I'm over 20 years old." );
+        if (age > 20) {
+            System.out.println("I'm over 20 years old.");
         }
 
         boolean maleGender = true;
         if (maleGender) {
-            System.out.println( "I'm a man." );
+            System.out.println("I'm a man.");
         }
 
         if (!maleGender) {
-            System.out.println( "I am not a man." );
+            System.out.println("I am not a man.");
         }
 
         short height = 184;
         if (height < 1.80) {
-            System.out.println( "I'm under 180." );
+            System.out.println("I'm under 180.");
         } else {
-            System.out.println( "I'm over 180." );
+            System.out.println("I'm over 180.");
         }
 
         char firstNameLetter = 'D';
         if (firstNameLetter == 'M') {
-            System.out.println( "My name begins with the letter M" );
+            System.out.println("My name begins with the letter M");
         } else if (firstNameLetter == 'I') {
-            System.out.println( "My name begins with the letter I" );
+            System.out.println("My name begins with the letter I");
         } else {
-            System.out.println( "My name does not begin with the letters M and I");
+            System.out.println("My name does not begin with the letters M and I");
         }
     }
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,20 @@
 public class Variable {   
     public static void main(String[] args) {
-        byte num = 32;
-        short num2 = 5418;
-        int num3 = 44484516;
-        long num4 = 1122334455;
-        float num5 = 3.484f;
-        double num6 = 3.46846848484d;
+        byte by = 32;
+        short sh = 5418;
+        int in = 44484516;
+        long lon = 1122334455;
+        float fl = 3.484f;
+        double db = 3.46846848484d;
         char sym = 't';
         boolean bl = true;
-        System.out.println (num);
-        System.out.println (num2);
-        System.out.println (num3);
-        System.out.println (num4);
-        System.out.println (num5);
-        System.out.println (num6);
-        System.out.println (sym);
-        System.out.println (bl);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(in);
+        System.out.println(lon);
+        System.out.println(fl);
+        System.out.println(db);
+        System.out.println(sym);
+        System.out.println(bl);
     }
 }
