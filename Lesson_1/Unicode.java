7cc8b1289fab163b86cb90be4280c66b7e584117
Geror125 <65406662+Geror125@users.noreply.github.com>
2021-04-03 19:23:42 +0300

    Update Unicode.java

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,9 +1,9 @@
 public class Unicode {
     public static void main(String[] args) {
         System.out.println("Unicode symbols and codes:\n# symbol");
-        for(int number = 33; number <= 128; number++){
+        for (int number = 33; number <= 126; number++) {
             char symbol = (char)number;
-            System.out.println(number + " - " + symbol);
+            System.out.println(number, symbol);
         }
     }
 }
