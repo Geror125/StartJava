62a5764397c2f4cd8dfbcd9c5561e728842a1d1c
Geror125 <65406662+Geror125@users.noreply.github.com>
2021-04-05 18:53:07 +0300

    MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,19 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        byte unknownNumber = 30;
-        byte userNumber = 30;
         System.out.println("TASK: Guess a number (from 0 to 100).");
-        System.out.println("Enter your number:" + userNumber);
         
-        if (userNumber > unknownNumber) {
-            System.out.println("This number is smaller than the one the computer riddled");
-        } else if (userNumber < unknownNumber) {
-            System.out.println("This number is greater than the number that the computer riddled");
-        } else {
-            System.out.println("Congratulations, the number has been guessed!");
+        byte unknownNumber = 23;
+        byte userNumber = 0;
+        while (userNumber != unknownNumber) {
+            System.out.println("Enter your number:" + userNumber);
+            if (userNumber > unknownNumber) {
+                userNumber -= 3;
+                System.out.println("This number is smaller than the one the computer riddled");
+            } else if (userNumber < unknownNumber) {
+                userNumber += 5;
+                System.out.println("This number is greater than the number that the computer riddled");
+            }
         }
+        System.out.println("Enter your number:" + userNumber + "\n" + "Congratulations, the number has been guessed!");
     }
 }
