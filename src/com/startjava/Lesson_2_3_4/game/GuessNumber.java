3702587f48b159e48a9d30a0f1c30cfd98792419
Geror125 <65406662+Geror125@users.noreply.github.com>
2021-05-06 01:57:30 +0300

    Проблемы с заполнением массива

--- src/com/startjava/Lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/Lesson_2_3_4/game/GuessNumber.java
@@ -1,15 +1,15 @@
 package com.startjava.Lesson_2_3_4.game;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
     Scanner scan = new Scanner(System.in);
 
-    private Player player1;
-    private Player player2;
-    private int targetNumber;
-    private int playerNumber;
+    private final Player player1;
+    private final Player player2;
+    private final int targetNumber;
 
     public GuessNumber(Player player1, Player player2) {
         Random number = new Random();
@@ -19,31 +19,48 @@ public class GuessNumber {
     }
 
     public void start() {
-        do {
-            System.out.print(player1.getName() + "\n  Enter your number:");
-            playerNumber = scan.nextInt();
-            if (compareNumbers()) {
-                break;
+        for (int i = 0; i < 10; i++) {
+            if (i < 9) {
+                System.out.println("You have " + (10-i) + " tries left");
+                System.out.print(player1.getName() + "\n  Enter your number:");
+                int[] player1Numbers = new int[10];
+                player1Numbers[i] = scan.nextInt();
+                if (player1Numbers[i] == targetNumber) {
+                    System.out.println("Player " + player1.getName() + " guessed number " + targetNumber + " from " + (i+1) + " tries");
+                    player1.setPlayerNumbers(player1Numbers);
+                    System.out.println(Arrays.toString(Arrays.copyOf(player1.getPlayerNumbers(), i+1)));
+                    break;
+                }
+                if (player1Numbers[i] > targetNumber) {
+                    System.out.println("This number is < than the one the computer riddled");
+                } else {
+                    System.out.println("This number is > than the number that the computer riddled");
+                }
+            } else {
+                System.out.println(player1.getName() + " has run out of tries.");
+                System.out.println(Arrays.toString(player1.getPlayerNumbers()));
             }
 
-            System.out.print(player2.getName() + "\n  Enter your number:");
-            playerNumber = scan.nextInt();
-            if (compareNumbers()) {
-                break;
+            if (i < 9) {
+                System.out.println("You have " + (10-i) + " tries left");
+                System.out.print(player2.getName() + "\n  Enter your number:");
+                int[] player2Numbers = new int[10];
+                player2Numbers[i] = scan.nextInt();
+                player2.setPlayerNumbers(new int[]{player2Numbers[i]});
+                if (player2Numbers[i] == targetNumber) {
+                    System.out.println("Player " + player2.getName() + " guessed number " + targetNumber + " from " + (i+1) + " tries");
+                    System.out.println(Arrays.toString(Arrays.copyOf(player2.getPlayerNumbers(), i+1)));
+                    break;
+                }
+                if (player2Numbers[i] > targetNumber) {
+                    System.out.println("This number is < than the one the computer riddled");
+                } else {
+                    System.out.println("This number is > than the number that the computer riddled");
+                }
+            } else {
+                System.out.println(player2.getName() + " has run out of tries.");
+                System.out.println(Arrays.toString(player2.getPlayerNumbers()));
             }
-        } while(true);
-        System.out.println("Winning!!!");
-    }
-
-    private boolean compareNumbers() {
-        if(playerNumber == targetNumber) {
-            return true;
         }
-        if (playerNumber > targetNumber) {
-            System.out.println("This number is < than the one the computer riddled");
-        } else {
-            System.out.println("This number is > than the number that the computer riddled");
-        }
-        return false;
     }
 }
--- src/com/startjava/Lesson_2_3_4/game/Player.java
+++ src/com/startjava/Lesson_2_3_4/game/Player.java
@@ -1,7 +1,8 @@
 package com.startjava.Lesson_2_3_4.game;
 
 public class Player {
-    private String name;
+    private final String name;
+    private int[] playerNumbers = new int[10];
 
     public Player(String name) {
         this.name = name;
@@ -10,4 +11,16 @@ public class Player {
     public String getName() {
         return name;
     }
+
+    public int[] getPlayerNumbers() {
+        return playerNumbers;
+    }
+
+    public int[] setPlayerNumbers(int[] playerNumbers) {
+        this.playerNumbers = playerNumbers;
+        return playerNumbers;
+    }
+//    public void setPlayerNumbers(int index, int number) {
+//        playerNumbers[index] = number;
+//    }
 }
